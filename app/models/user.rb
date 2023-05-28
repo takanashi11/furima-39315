55be20ab1c8bf820ac5a030f59fcc7037f70e502
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-05-28 21:03:48 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable 
+
+         validates :nickname, presence: true
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+         validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
+         validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
+         validates :katakana_surname, presence: true, format: { with: /\A[\p{Katakana}\p{Space}ー－]+\z/}
+         validates :katakana_name, presence: true, format: { with: /\A[\p{Katakana}\p{Space}ー－]+\z/}
+  has_many :items
+  has_many :purchases
+
+
+  
+
+
 end
