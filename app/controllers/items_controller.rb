0b5cd96453b8803c49f8895a7732c437929f7bbe
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-05-30 16:41:15 +0900

    出品機能　調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
- # before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     #@item = Item.all
@@ -17,15 +17,15 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+end
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :image, :item_text, :category_id, :detail_id, :prefecture_id, :redion_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :image, :item_text, :category_id, :detail_id, :profecture_id, :region_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
-  #def move_to_index
-    #unless user_signed_in?
-      #redirect_to action: :index
-   # end
- # end
-end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
