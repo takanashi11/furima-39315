a98a264acce38db3ae3e97f2fae9e017fb8d89f0
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-06-01 10:55:50 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,11 +50,11 @@ class ItemsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to new_user_session_path
       return
     end
     unless @item.nil? ||@item.user.nil? || current_user.id == @item.user.id
-      redirect_to action: :index
+      redirect_to new_user_session_path
      end
     
   end
